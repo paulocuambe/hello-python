7371f892c65a5355bfb4a1e84ffbf933e229d3d1
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-11-19 15:13:28 +0200

    add operator exercises

---
+++ exercises/operators.py
@@ -0,0 +1,18 @@
+a = 93
+b = 3
+
+print('Addition', a + b)
+print('Subtraction', a - b)
+print('Multiplication', a * b)
+print('Division', a / b)
+print('Modulus', a % b)
+print('Exponentiation', a**b)
+print('Floor Division', a // b)
+
+print("\n\n\n-------Assignment Operators-------\n\n\n")
+
+c = 4
+d = 7
+d += c
+print(d)  # 11
+print(True ^ True)  # XOR - Is only true if only of them is true
