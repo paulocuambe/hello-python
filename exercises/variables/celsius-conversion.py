616eadd0036049ce0b47041b1286a0fecf085869
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-11-28 19:36:06 +0200

    add celsius conversion exercise

---
+++ exercises/variables/celsius-conversion.py
@@ -0,0 +1,6 @@
+celsius = float(input("What is the temperature in celsius? "))
+# (celsius * 9/5) + 32
+fahrenheit = celsius * 9 / 5 + 32
+kelvin = celsius + 273.15
+print("Fahrenheit", fahrenheit, "F")
+print("Kelvin", kelvin, "K")
