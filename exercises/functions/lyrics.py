2fb16fcb722b1de7c6e702e6d7390bb5df9faa65
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-11-30 10:55:14 +0200

    more practice

---
+++ exercises/functions/lyrics.py
@@ -0,0 +1,17 @@
+def lyrics():
+    print("All Hail King Jesus")
+    print("He Is the All Might")
+    print("He owns all the power and majesty")
+    print("")
+    print("Since The beginning He Is")
+    print("He didn't change")
+    print("All Praise to King Jesus.")
+    print("")
+    print("Hosana, Hosana, Hosana")
+    print("Jesus, Jesus, The King")
+    print("Oh Oh Oh")
+    print("")
+    print("A lyric invented by Paulo while practicing Python")
+
+
+lyrics()
---
+++ exercises/math/basic.py
@@ -0,0 +1,8 @@
+import math
+import random
+
+print(math.log(1200, 10))
+print(math.pi)
+
+print(random.randint(1, 9999))
+print(random.choice([1, 10, 40, 2, -2]))
