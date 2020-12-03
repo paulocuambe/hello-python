ed1a63c6fbc2de20bf7a3880735251f92b890745
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-12-03 19:19:40 +0200

    improve conditionals

--- exercises/conditionals/student_qualification.py
+++ exercises/conditionals/student_qualification.py
@@ -3,13 +3,17 @@ phone = input("What's your phone number? [without +]: ")
 country = input("Country of birth: ")
 gender = input("Gender [male/female]: ")
 year_birth = int(input("Year of birth: "))
-type_student = input("Profession?[student/working]")
+type_student = input("Studying or working? [student/working]: ")
 
+print()
 score = 0
+
 for i in range(1, 5):
-    score += float(input(f"Score in quizz {i}: "))
+    score += float(input(f"Your score in quizz {i}: "))
+print()
 
 has_20 = (2020 - year_birth) > 20
+startwith_25 = phone.startswith("25")
 
 is_african = country.lower() in ["uganda", "kenya", "mozambique", "nigeria"]
 is_student = type_student.lower() == "student"
@@ -18,7 +22,10 @@ is_female = gender.lower() == "female"
 is_male = gender.lower() == "male"
 
 avg_score = score / 4
-startwith_25 = phone.startswith("25")
+
+if not is_student:
+    print("You don't meet the treshold")
+    exit()
 
 if is_african and is_female and has_20 and startwith_25 and avg_score >= 75:
     print("You qualify for a sholarship.")
@@ -26,11 +33,3 @@ elif is_african and is_male and has_20 and startwith_25 and avg_score >= 80:
     print("You qualify for a sholarship.")
 else:
     print("You don't meet the treshold.")
-"""
-if type_student not in ["yes", "no"]:
-  exit("You provided an invalid number")
-
-try:
-except:
-  exit("You provided an invalid year of birth. Try again.")
-"""
