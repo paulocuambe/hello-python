821dfcbe97181492748874308576c404c84d272b
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-12-07 16:55:24 +0200

    add logic to say who sent most emails

--- exercises/dictionaries/email_histogram.py
+++ exercises/dictionaries/email_histogram.py
@@ -6,9 +6,16 @@ except:
     exit(f"Could not open file {fname}")
 
 emails = dict()
+person = ""
+nr_emails = 0
 for line in fhand:
     if line.startswith("From "):
         email = line.rstrip().split()[1]
         emails[email] = emails.get(email, 0) + 1
 
+        if emails[email] > nr_emails:
+            person = email
+            nr_emails = emails[email]
+
 print(emails)
+print(f"{person} sent {nr_emails}")
